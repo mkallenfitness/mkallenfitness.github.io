ed37ecb5c41d42d7bdc7acb12e47d8455cd9ca01
mkallenfitness <78892226+mkallenfitness@users.noreply.github.com>
2024-07-12 21:24:37 -0400

    Update scripts.js

--- condo/scripts.js
+++ condo/scripts.js
@@ -1,7 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
     const unavailableDateRanges = [
-        { start: "2024-07-12", end: "2024-07-19" },
-        { start: "2024-07-25", end: "2024-08-09" },
+        { start: "2024-07-12", end: "2024-08-09" },
     ];
 
     const calendarContainer = document.getElementById("calendar-container");
